54cab7efde89592daf8e6de9ffef29b09df73da7
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-03-28 09:54:24 +1100

    Update main.go

--- main.go
+++ main.go
@@ -11,3 +11,20 @@ import (
 	
 	task "github.com/danielaltamirano1993/go-crud-cli/tasks"
 )
+
+func main() {
+	// read or create the tasks.json file
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var tasks []task.Task
+
+	// get file info
+	info, err := file.Stat()
+
+	if err != nil {
+		panic(err)
+	}
